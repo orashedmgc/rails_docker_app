ce602ea6f4884ae21790a0edf8e1081247ef9b5a
orashedmgc <orashedmgc@gmail.com>
2024-09-24 18:19:52 +0300

    Add staging environment

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module RailsDockerApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.autoload_paths += %W(#{config.root}/app/jobs)
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
